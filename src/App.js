447c929234db5260c6362229cd95d65979e47b5e
Vanel <vanelnwaba@gmail.com>
2023-04-06 15:23:44 +0100

    configure routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import Login from './pages/Login';
+import Home from './pages/Home';
+import RegisterScreen from './pages/Register';
 
 function App() {
+  const { isAuthenticated } = useSelector((state) => state.auth);
+
   return (
-    <div className="App">
-      Final Capstone
-    </div>
+    <Router>
+      <ToastContainer />
+      <Routes>
+        <Route path="/" element={!isAuthenticated ? <Login /> : <Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<RegisterScreen />} />
+      </Routes>
+    </Router>
   );
 }
 
