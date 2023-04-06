0aeb2e1bbabfd19cecf89b798310e99b5c097adb
Vanel <vanelnwaba@gmail.com>
2023-04-06 15:25:08 +0100

    add store  and slice

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
+import authReducer from '../features/auth/authSlice';
 
 const store = configureStore({
   reducer: {
-
+    auth: authReducer,
   },
 });
 
