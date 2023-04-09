1056cc7dc1a02c25862dce0633d7e3ccf0e8e576
Ezema Anthony Sunday <tonyunday@gmail.com>
2023-04-09 05:34:36 -0700

    Define route for addcar form using react router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import React from 'react';
-import './App.css';
+// import React from 'react';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       Final Capstone
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import AddCar from './components/AddCar';
 
 function App() {
   return (
-    <div className="App">
-      Final Capstone
-    </div>
+    <Router>
+      <Route exact path="/AddCar">
+        <AddCar />
+      </Route>
+    </Router>
   );
 }
-
 export default App;
