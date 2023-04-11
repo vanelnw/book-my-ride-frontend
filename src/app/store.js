6c9f80ea285e592020982ec94c4a72974344392d
Khomotso Mkansi <kgomotsomkansi@gmail.com>
2023-04-12 01:05:09 +0200

    Updated store.js to include carReducer

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from '../features/auth/authSlice';
+import cardataReducer from '../Redux/carsList/carsListSlice';
 
 const store = configureStore({
   reducer: {
+    cardata: cardataReducer,
     auth: authReducer,
   },
 });
