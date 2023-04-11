26561f5a6e83512b87acad49e2174dc542806786
Ezema Anthony Sunday <tonyunday@gmail.com>
2023-04-10 20:41:34 -0700

    Fix linters error

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import AddCar from './components/AddCar';
-// import { fetchCars } from './features/car/carActions';
 
 function App() {
   // const { isAuthenticated } = useSelector((state) => state.auth);
