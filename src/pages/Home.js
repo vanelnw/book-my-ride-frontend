d0e63f340fbb91e4f764a3b692a5627ee95b0d4e
Khomotso Mkansi <kgomotsomkansi@gmail.com>
2023-04-12 01:03:58 +0200

    Added Homepage component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,21 +1,55 @@
-import React from 'react';
-import { useDispatch } from 'react-redux';
-import { logout } from '../features/auth/authSlice';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { fetchItems } from '../Redux/carsList/carsListSlice';
+import './Home.css';
 
-function Home() {
+const Home = () => {
   const dispatch = useDispatch();
-  const handleLogout = (e) => {
-    e.preventDefault();
-    dispatch(logout());
-  };
+  const { loading, error, items } = useSelector((state) => state.cardata);
+
+  useEffect(() => {
+    dispatch(fetchItems());
+  }, [dispatch]);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return (
+      <div>
+        Error:
+        {error}
+      </div>
+    );
+  }
+
   return (
-    <div>
-      Home
-      <button type="button" className="button" onClick={handleLogout}>
-        Logout
-      </button>
+    <div className="container">
+      <div className="">
+        <h1>Latest Models</h1>
+        <span>Please select a car</span>
+      </div>
+      <div className="cars-container">
+        {items.map((item) => (
+          <div className="car-item" key={item.id}>
+            <img className="car-img" src={item.image} alt="car" />
+            <div className="car-details">
+              <h2>{item.make}</h2>
+              <h3>{item.model}</h3>
+              <p className="price">
+                $
+                {item.price}
+              </p>
+
+              <Link to={`/car/${item.id}`} className="btn-details">View Details</Link>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
-}
+};
 
 export default Home;
