7c9052ba6dcad0a3b0787dfd73dfb9882135982d
Vanel <vanelnwaba@gmail.com>
2023-04-12 16:59:45 +0100

    update carSlice and actions

--- src/app/store.js
+++ src/app/store.js
@@ -1,13 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from '../features/auth/authSlice';
-import carReducer from '../features/cars/carSlice';
-import cardataReducer from '../Redux/carsList/carsListSlice';
+import carReducer from '../features/car/carSlice';
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
     cars: carReducer,
-    cardata: cardataReducer,
   },
 });
 
