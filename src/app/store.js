3d991d7b7ac8ad1dfa75e1fccc74bcc83625e9e1
Vanel <vanelnwaba@gmail.com>
2023-04-12 06:35:24 +0100

    update store

--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from '../features/auth/authSlice';
+import carReducer from '../features/cars/carSlice';
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
+    cars: carReducer,
   },
 });
 
